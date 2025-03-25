cd4805602efcd451236e2c7df0205bfe7b16c363
Adheesha Chamod <adheeshachamoddesilva@gmail.com>
2025-03-25 21:29:18 +0530

    Add User bean

--- src/main/java/org/adheesha/Main.java
+++ src/main/java/org/adheesha/Main.java
@@ -8,8 +8,16 @@ public class Main {
     public static void main(String[] args) {
 
         ApplicationContext context = new ClassPathXmlApplicationContext("configuration.xml");
+//        printBeans(context);
 
-        User user = context.getBean(User.class);
+        User user = (User) context.getBean("user");
         user.sayHello();
+    }
+
+    public static void printBeans(ApplicationContext context) {
+        String[] names = context.getBeanDefinitionNames();
+        for (String name : names) {
+            System.out.println(name);
+        }
     }
 }
--- src/main/java/org/adheesha/User.java
+++ src/main/java/org/adheesha/User.java
@@ -2,6 +2,10 @@ package org.adheesha;
 
 public class User {
 
+    User () {
+        System.out.println("User object is created!");
+    }
+
     public void sayHello() {
         System.out.println("Hello, World!");
     }
