f7b069a02597abd7ec2b5b479b9902fa0533263c
Adheesha Chamod <adheeshachamoddesilva@gmail.com>
2025-03-30 18:53:43 +0530

    Add lazy initialization

--- src/main/java/org/adheesha/BackupDBConnection.java
+++ src/main/java/org/adheesha/BackupDBConnection.java
@@ -2,6 +2,10 @@ package org.adheesha;
 
 public class BackupDBConnection implements DBConnection {
 
+    public BackupDBConnection() {
+        System.out.println("BackupDBConnection object is created!");
+    }
+
     @Override
     public void connect() {
         System.out.println("Connected to Backup Database");
--- src/main/java/org/adheesha/PrimaryDBConnection.java
+++ src/main/java/org/adheesha/PrimaryDBConnection.java
@@ -2,6 +2,10 @@ package org.adheesha;
 
 public class PrimaryDBConnection implements DBConnection {
 
+    public PrimaryDBConnection() {
+//        System.out.println("PrimaryDBConnection object is created!");
+    }
+
     @Override
     public void connect() {
         System.out.println("Connected to Primary Database");
