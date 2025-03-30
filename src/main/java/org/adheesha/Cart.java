470b03bf81e2b4d1470c2163a0810231cfe3ad5f
Adheesha Chamod <adheeshachamoddesilva@gmail.com>
2025-03-30 21:35:15 +0530

    Add inner bean

--- src/main/java/org/adheesha/Cart.java
+++ src/main/java/org/adheesha/Cart.java
@@ -2,11 +2,17 @@ package org.adheesha;
 
 public class Cart {
 
+    private Item item;
+
 //    Cart() {
 //        System.out.println("Cart object is created!");
 //    }
 
     public void addToCart() {
         System.out.println("Item added to cart");
+    }
+
+    public void setItem(Item item) {
+        this.item = item;
     }
 }
---
+++ src/main/java/org/adheesha/Item.java
@@ -0,0 +1,8 @@
+package org.adheesha;
+
+public class Item {
+
+    Item() {
+        System.out.println("Item object created!");
+    }
+}
